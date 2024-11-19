536b39f1a22a360f3d41da5f8fec112a40b86d74
MateoBaravalle <mateobara2@gmail.com>
2024-11-19 13:26:57 -0300

    Mannage Unauthoriced entrys

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,17 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        if ($request->is('api/*')) {
+            abort(401, 'Unauthenticated');
+        }
+
         if (! $request->expectsJson()) {
             return route('login');
         }
+    }
+
+    protected function unauthenticated($request, array $guards)
+    {
+        abort(401, 'Unauthenticated');
     }
 }

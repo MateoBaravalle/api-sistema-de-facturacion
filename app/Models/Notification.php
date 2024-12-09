796b0df36200c3c95e2468340b926bcdc07ad36e
MateoBaravalle <mateobara2@gmail.com>
2024-12-09 15:14:44 -0300

    Refactor model relationships to include type hints for Eloquent relationships
    - Updated the Invoice, Notification, Order, PriceHistory, and Role models to specify return types for relationship methods using Eloquent's BelongsTo, BelongsToMany, HasOne, and MorphTo.
    - Improved code clarity and type safety by explicitly defining relationship return types.

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Notification extends Model
 {
@@ -26,7 +27,7 @@ class Notification extends Model
         'updated_at' => 'datetime',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
